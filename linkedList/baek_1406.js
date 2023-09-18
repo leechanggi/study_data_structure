6735361ca7df151ba248775d8955979fb53305dd
Chaplin <67743720+leechanggi@users.noreply.github.com>
2023-09-18 11:59:30 +0900

    백준 1406

--- linkedList/baek_1406.js
+++ linkedList/baek_1406.js
@@ -1,65 +1,32 @@
-let example = `abcd
-3
-P x
-L
-P y`;
+// let example = `dmih
+// 11
+// B
+// B
+// P x
+// L
+// B
+// B
+// B
+// P y
+// D
+// D
+// P z`;
+// let input = example.toString().split("\n");
 
-let input = example.trim().split("\n");
-let [str, n, ...commands] = input;
-let values = str.trim().split("");
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+let left = input[0].split("");
+let right = [];
+let n = parseInt(input[1]);
 
-const findCursor = value => {
-	return value.indexOf("|");
-};
+for (let i = 2; i < n + 2; i++) {
+	let [c, v] = input[i].split(" ");
+	if (c === "L" && left.length) right.push(left.pop());
+	else if (c === "D" && right.length) left.push(right.pop());
+	else if (c === "B") left.pop();
+	else if (c === "P") left.push(v);
+}
 
-const moveLeft = (value, index) => {
-	if (index > 0) {
-		value.splice(index, 1);
-		value.splice(index - 1, 0, "|");
-	}
-	return value;
-};
-
-const moveRight = (value, index) => {
-	if (index > 0) {
-		value.splice(index, 1);
-		value.splice(index + 1, 0, "|");
-	}
-	return value;
-};
-
-const removeStr = (value, index) => {
-	if (index > 0) {
-		value.splice(index, 1);
-		value.splice(index - 1, 1, "|");
-	}
-	return value;
-};
-
-const addStr = (value, index, str) => {
-	if (index > 0) {
-		value.splice(index, 1);
-		value.splice(index, 0, str);
-		value.splice(index + 1, "|");
-	} else {
-	}
-	return value;
-	// index가 0일 경우 무시
-	// index + 1, splice
-};
-
-const removeCursor = () => {};
-
-// 인자값은 항상 전달해야함. 마지막의 반환은 |을 삭제
-// 시간복잡도를 O(n)으로
-
-values.push("|");
-console.log(values);
-values = moveLeft(values, findCursor(values));
-console.log(values);
-values = moveRight(values, findCursor(values));
-console.log(values);
-values = removeStr(values, findCursor(values));
-console.log(values);
-values = addStr(values, findCursor(values), "z");
-console.log(values);
+let answer = left.join("");
+answer += right.reverse().join("");
+console.log(answer);
