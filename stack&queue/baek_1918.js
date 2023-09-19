dfdb80fbf71095f8c33db25fc69ae099c1facba8
Chaplin <67743720+leechanggi@users.noreply.github.com>
2023-09-19 11:08:10 +0900

    Update baek_1918.js

--- stack&queue/baek_1918.js
+++ stack&queue/baek_1918.js
@@ -1,7 +1,46 @@
-let example = `A*(B+C)`;
-let input = example.toString().replace(/\s+/g, "");
-let stack = [];
+// let example = `A*(B+C)`;
+// let input = example.toString().trim().split("\n");
 
-// 공백 제거, [A,*,[B,+,C]]
+const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require("fs").readFileSync(path).toString().trim().split("\n");
 
-console.log(input);
+let [string] = input;
+
+function calc(string) {
+	const split = string.split("");
+	const stack = [];
+	let answer = "";
+
+	for (let i = 0; i < split.length; i++) {
+		const e = split[i];
+
+		if (e === "(") {
+			stack.push(e);
+		} else if (e === ")") {
+			while (stack.length && stack[stack.length - 1] !== "(") {
+				answer += stack.pop();
+			}
+			stack.pop();
+		} else if (e === "*" || e === "/") {
+			while ((stack.length && stack[stack.length - 1] === "*") || stack[stack.length - 1] === "/") {
+				answer += stack.pop();
+			}
+			stack.push(e);
+		} else if (e === "+" || e === "-") {
+			while (stack.length && stack[stack.length - 1] !== "(") {
+				answer += stack.pop();
+			}
+			stack.push(e);
+		} else {
+			answer += e;
+		}
+	}
+
+	while (stack.length) {
+		answer += stack.pop();
+	}
+	return answer;
+}
+
+const answer = calc(string);
+console.log(answer);
